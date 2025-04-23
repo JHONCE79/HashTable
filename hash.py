70ae7380b1da6df1768f4daaf89af5f8c99f3199
JHONCE79 <jhonsteven.163@gmail.com>
2025-04-23 17:39:58 -0500

    different functions complete

---
+++ hash.py
@@ -0,0 +1,34 @@
+class HashTable:
+    def __init__(self, size=10):
+        self.size = size
+        self.table = [None] * self.size
+
+
+    def hash_multiplicativa(self,key):
+        aurea = 0.6180339887
+        hash_value = int(hash(key) * aurea) % self.size
+        return hash_value
+    
+    def hash_XOR(self,key):
+        hash_value = 0
+        for i in key:
+            hash_value ^= hash_value << 3 + hash_value >>7 + ord(i)
+        return hash_value
+
+    def hash_per(self,key):
+        byte_data = str(key).encode()
+        byte_sum = sum(byte_data)
+        return (byte_sum * 31) % self.size
+
+    def insert(self,key,value):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def search(self, key):
+        pass
+
+    def __str__(self):
+        return str(self.table)
+    
